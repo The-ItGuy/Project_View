5cee6ec11dd8866bb4c9da7feba769775319b195
Ashish <42575765+The-ItGuy@users.noreply.github.com>
2023-10-04 20:41:42 +0530

    Update data-contracts.ts

--- fe/src/app/api/data-contracts.ts
+++ fe/src/app/api/data-contracts.ts
@@ -1,13 +1,4 @@
-/* eslint-disable */
-/* tslint:disable */
-/*
- * ---------------------------------------------------------------
- * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
- * ##                                                           ##
- * ## AUTHOR: acacode                                           ##
- * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
- * ---------------------------------------------------------------
- */
+
 
 export interface SuccessDto {
   message: string;
