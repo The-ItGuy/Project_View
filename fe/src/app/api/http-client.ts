b6e8d41fab1ad43cb24ea351488d886b3229fefd
Ashish <42575765+The-ItGuy@users.noreply.github.com>
2023-10-04 20:41:57 +0530

    Update http-client.ts

--- fe/src/app/api/http-client.ts
+++ fe/src/app/api/http-client.ts
@@ -1,13 +1,4 @@
-/* eslint-disable */
-/* tslint:disable */
-/*
- * ---------------------------------------------------------------
- * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
- * ##                                                           ##
- * ## AUTHOR: acacode                                           ##
- * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
- * ---------------------------------------------------------------
- */
+
 
 import type {
   AxiosInstance,
